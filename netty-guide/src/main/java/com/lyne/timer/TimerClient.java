03e0942871caf3d74c4f46aae71efa87c4a8c98d
lyneliu <595596226@qq.com>
2017-08-24 15:08:17 +0800

    update

--- netty-guide/src/main/java/com/lyne/timer/TimerClient.java
+++ netty-guide/src/main/java/com/lyne/timer/TimerClient.java
@@ -34,32 +34,30 @@ public class TimerClient {
         try {
 
             for (int i = 0; i < 100; i++) {
-                threadPoolExecutor.submit(new Runnable() {
-                    @Override public void run() {
-                        try {
-                            Bootstrap b = new Bootstrap(); // (1)
-                            b.group(workerGroup); // (2)
-                            b.channel(NioSocketChannel.class); // (3)
-                            b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
-                            b.handler(new ChannelInitializer<SocketChannel>() {
-                                @Override public void initChannel(SocketChannel ch)
-                                        throws Exception {
-                                    ch.pipeline().addLast(new TimeClientHandler());
-                                }
-                            });
+                threadPoolExecutor.submit(() -> {
+                    try {
+                        Bootstrap b = new Bootstrap(); // (1)
+                        b.group(workerGroup); // (2)
+                        b.channel(NioSocketChannel.class); // (3)
+                        b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+                        b.handler(new ChannelInitializer<SocketChannel>() {
+                            @Override public void initChannel(SocketChannel ch)
+                                    throws Exception {
+                                ch.pipeline().addLast(new TimeClientHandler());
+                            }
+                        });
 
-                            // 启动客户端
-                            ChannelFuture f = b.connect(host, port).sync(); // (5)
+                        // 启动客户端
+                        ChannelFuture f = b.connect(host, port).sync(); // (5)
 
-                            // 等待连接关闭
-                            f.channel().closeFuture().sync();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        } finally {
-                            count++;
-                            LogUtil.warn("exec num:" + count);
-                            workerGroup.shutdownGracefully();
-                        }
+                        // 等待连接关闭
+                        f.channel().closeFuture().sync();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        count++;
+                        LogUtil.warn("exec num:" + count);
+                        workerGroup.shutdownGracefully();
                     }
                 });
             }
